bb65fc863817676cce0311072eb06a8a30707216
nschaetti <n.schaetti@gmail.com>
2018-10-15 21:18:01 +0200

    RNN

--- models/RNN.py
+++ models/RNN.py
@@ -7,6 +7,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.nn.utils as utils
+from torch.autograd import Variable
 
 
 # RNN
@@ -16,7 +18,7 @@ class RNN(nn.Module):
     """
 
     # Constructor
-    def __init__(self, input_dim, hidden_dim, n_authors=15, rnn_type='lstm', num_layers=1, dropout=True):
+    def __init__(self, input_dim, hidden_dim, n_authors=15, rnn_type='lstm', num_layers=1, dropout=True, batch_size=64):
         """
         Constructor
         :param input_dim:
@@ -31,6 +33,8 @@ class RNN(nn.Module):
         self.hidden_dim = hidden_dim
         self.num_layers = num_layers
         self.rnn_type = rnn_type
+        self.batch_size = batch_size
+        self.n_authors = n_authors
 
         # RNN
         if rnn_type == 'lstm':
@@ -45,44 +49,84 @@ class RNN(nn.Module):
         self.hidden2outputs = nn.Linear(hidden_dim, n_authors)
 
         # Init hiddens
-        self.hidden = self.init_hidden()
+        self.hidden = self.init_hidden(batch_size)
     # end __init__
 
     # Init hidden state
-    def init_hidden(self):
+    def init_hidden(self, batch_size):
         """
         Init hidden state
         (num_layers, minibatch_size, hidden_dim)
         :return:
         """
         if self.rnn_type == 'lstm':
-            return (torch.zeros(1, self.num_layers, self.hidden_dim),
-                    torch.zeros(1, self.num_layers, self.hidden_dim))
+            # Hidden and cell
+            hidden = torch.zeros(self.num_layers, batch_size, self.hidden_dim)
+            cell = torch.zeros(self.num_layers, batch_size, self.hidden_dim)
+
+            # To GPU
+            if next(self.parameters()).is_cuda:
+                hidden = hidden.cuda()
+                cell = cell.cuda()
+            # end if
+
+            # To Variable
+            hidden = Variable(hidden)
+            cell = Variable(cell)
+
+            return hidden, cell
         else:
-            return torch.zeros(1, self.num_layers, self.hidden_dim)
+            # Hidden
+            hidden = torch.zeros(self.num_layers, batch_size, self.hidden_dim)
+
+            # To GPU
+            if next(self.parameters()).is_cuda:
+                hidden = hidden.gpu()
+            # end if
+
+            # To variable
+            hidden = Variable(hidden)
+
+            return hidden
         # end if
     # end init_hidden
 
     # Forward
-    def forward(self, x):
+    def forward(self, x, x_lengths):
         """
         Forward pass
         :param x:
         :return:
         """
+        # Sizes
+        batch_size, seq_len, embedding_dim = x.size()
+
+        # Init hiddens
+        self.hidden = self.init_hidden(batch_size)
+
+        # Pack to hide padded item to RNN
+        x = utils.rnn.pack_padded_sequence(x, x_lengths, batch_first=True)
+
         # Exec. RNN
-        rnn_out, self.hidden = self.rnn(x, self.hidden)
+        x, self.hidden = self.rnn(x, self.hidden)
+
+        # Undo packing
+        x, _ = utils.rnn.pad_packed_sequence(x, batch_first=True)
 
         # View to (length, output size)
-        rnn_out = rnn_out.view((x.size(0), -1))
+        x = x.contiguous()
+        x = x.view((-1, self.hidden_dim))
 
-        # Author space
-        outputs = self.hidden2outputs(rnn_out)
+        # Linear layer
+        x = self.hidden2outputs(x)
 
         # Author scores
-        author_scores = F.log_softmax(outputs, dim=1)
+        x = F.log_softmax(x, dim=1)
 
-        return author_scores
+        # Back to (batch_size, seq_len, nb_tags)
+        x = x.view(batch_size, seq_len, self.n_authors)
+
+        return x
     # end forward
 
 # end EmbRNN
