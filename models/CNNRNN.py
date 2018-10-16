42654f3fbc720dc74c56e8975995af10d285251c
nschaetti <n.schaetti@gmail.com>
2018-10-16 22:24:29 +0200

    CNN-RNN

--- models/CNNRNN.py
+++ models/CNNRNN.py
@@ -7,6 +7,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.autograd import Variable
+import torch.nn.utils as utils
 
 
 # RNN with CNN inputs
@@ -16,7 +18,7 @@ class CNNRNN(nn.Module):
     """
 
     # Constructor
-    def __init__(self, embedding_dim, vocab_size, text_length, hidden_dim, n_authors=15, out_channels=(50, 50, 50), kernel_sizes=(3, 4, 5), rnn_type='lstm', num_layers=1, dropout=True):
+    def __init__(self, embedding_dim, vocab_size, text_length, hidden_dim, n_authors=15, out_channels=(50, 50, 50), kernel_sizes=(3, 4, 5), rnn_type='lstm', num_layers=1, dropout=True, batch_size=64):
         """
         Constructor
         :param embedding_dim:
@@ -35,11 +37,14 @@ class CNNRNN(nn.Module):
 
         # Properties
         self.hidden_dim = hidden_dim
+        self.embedding_dim = embedding_dim
         self.num_layers = num_layers
         self.rnn_type = rnn_type
         self.text_length = text_length
         self.out_channels = out_channels
         self.kernel_sizes = kernel_sizes
+        self.batch_size = batch_size
+        self.n_authors = n_authors
 
         # Embeddings
         self.embedding_layer = nn.Embedding(vocab_size, embedding_dim)
@@ -77,41 +82,75 @@ class CNNRNN(nn.Module):
         self.hidden2outputs = nn.Linear(hidden_dim, n_authors)
 
         # Init hiddens
-        self.hidden = self.init_hidden()
+        self.hidden = self.init_hidden(batch_size)
     # end __init__
 
     # Init hidden state
-    def init_hidden(self):
+    def init_hidden(self, batch_size):
         """
         Init hidden state
         (num_layers, minibatch_size, hidden_dim)
         :return:
         """
         if self.rnn_type == 'lstm':
-            return (torch.zeros(1, self.num_layers, self.hidden_dim),
-                    torch.zeros(1, self.num_layers, self.hidden_dim))
+            # Hidden and cell
+            hidden = torch.zeros(self.num_layers, batch_size, self.hidden_dim)
+            cell = torch.zeros(self.num_layers, batch_size, self.hidden_dim)
+
+            # To GPU
+            if next(self.parameters()).is_cuda:
+                hidden = hidden.cuda()
+                cell = cell.cuda()
+            # end if
+
+            # To Variable
+            hidden = Variable(hidden)
+            cell = Variable(cell)
+
+            return hidden, cell
         else:
-            return torch.zeros(1, self.num_layers, self.hidden_dim)
+            # Hidden
+            hidden = torch.zeros(self.num_layers, batch_size, self.hidden_dim)
+
+            # To GPU
+            if next(self.parameters()).is_cuda:
+                hidden = hidden.gpu()
+            # end if
+
+            # To variable
+            hidden = Variable(hidden)
+
+            return hidden
         # end if
     # end init_hidden
 
     # Forward
-    def forward(self, x):
+    def forward(self, x, x_lengths, reset_hidden=True):
         """
         Forward pass
         :param x:
         :return:
         """
+        # Sizes
+        batch_size, seq_len, input_dim = x.size()
+
+        # Init hiddens
+        if reset_hidden:
+            self.hidden = self.init_hidden(batch_size)
+        # end if
+
         # Embeddings
-        embeds = self.embeddings(x)
+        x = self.embedding_layer(x)
 
         # Add channel dim
-        embeds = torch.unsqueeze(embeds, dim=1)
+        x = x.contiguous()
+        x = x.view(-1, self.text_length, self.embedding_dim)
+        x = x.unsqueeze(1)
 
         # Conv window
-        out_win1 = F.relu(self.conv_w1(embeds))
-        out_win2 = F.relu(self.conv_w2(embeds))
-        out_win3 = F.relu(self.conv_w3(embeds))
+        out_win1 = F.relu(self.conv_w1(x))
+        out_win2 = F.relu(self.conv_w2(x))
+        out_win3 = F.relu(self.conv_w3(x))
 
         # Remove last dim
         out_win1 = torch.squeeze(out_win1, dim=3)
@@ -124,21 +163,34 @@ class CNNRNN(nn.Module):
         max_win3 = self.max_pool_w3(out_win3)
 
         # Concatenate
-        out = torch.cat((max_win1, max_win2, max_win3), dim=1)
+        x = torch.cat((max_win1, max_win2, max_win3), dim=1)
+
+        # Back to (batch_size, seq_len, n_filters)
+        x = x.view(batch_size, seq_len, -1)
+
+        # Pack to hide padded item to RNN
+        x = utils.rnn.pack_padded_sequence(x, x_lengths, batch_first=True)
 
         # Exec. RNN
-        rnn_out, self.hidden = self.rnn(out, self.hidden)
+        x, self.hidden = self.rnn(x, self.hidden)
+
+        # Undo packing
+        x, _ = utils.rnn.pad_packed_sequence(x, batch_first=True)
 
         # View to (length, output size)
-        rnn_out = rnn_out.view((x.size(0), -1))
+        x = x.contiguous()
+        x = x.view((-1, self.hidden_dim))
 
         # Author space
-        outputs = self.hidden2outputs(rnn_out)
+        x = self.hidden2outputs(x)
 
         # Author scores
-        author_scores = F.log_softmax(outputs, dim=1)
+        x = F.log_softmax(x, dim=1)
 
-        return author_scores
+        # Back to (batch_size, seq_len, nb_tags)
+        x = x.view(batch_size, seq_len, self.n_authors)
+
+        return x
     # end forward
 
 # end EmbRNN
