27246353799bfd34bec7bd8bb51e5936db904419
nschaetti <n.schaetti@gmail.com>
2019-03-19 14:31:24 +0100

    models __init__

--- models/__init__.py
+++ models/__init__.py
@@ -6,3 +6,4 @@ from CNNRNN import CNNRNN
 from EmbRNN import EmbRNN
 from PretrainedRNN import PretrainedRNN
 from RNN import RNN
+from KerasRNN import create_rnn_model_pretrained_embedding, create_rnn_model_with_embedding, create_stanford_article_level_model, GlobalAveragePooling1DMasked
