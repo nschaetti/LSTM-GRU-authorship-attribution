bf4c6dea44e70abb6f33ecb8f56ba0cab8cd4750
nschaetti <n.schaetti@gmail.com>
2019-05-18 17:21:54 +0200

    Balloon and D10

--- scripts/keras_wv_p_balloon.sh
+++ scripts/keras_wv_p_balloon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python2.7 model_balloon_keras.py --hidden-size 100 --cell-size 100 --embedding-size 400 --feature wv --precomputed-features pca100 --learning-window 40 --max-length 40 --k 10 --output outputs/ --name "AA Na15 Balloon 100-100 WV-P ESN-100 keras" --description "Authorship Attribution 15 authors Balloon 100 hidden neurons, Word Vectors (WV) pretrained with Glove, ESN reduced to 100" --rnn-type LSTM --epoch 250 --batch-size 64 --pretrained --verbose 4 --cuda
+# python2.7 model_balloon_keras.py --hidden-size 100 --cell-size 100 --embedding-size 400 --feature wv --precomputed-features pca100 --learning-window 40 --max-length 40 --k 10 --output outputs/ --name "AA Na15 Balloon 100-100 WV-P ESN-100 keras" --description "Authorship Attribution 15 authors Balloon 100 hidden neurons, Word Vectors (WV) pretrained with Glove, ESN reduced to 100" --rnn-type LSTM --epoch 250 --batch-size 64 --pretrained --verbose 4 --cuda
 
 python2.7 model_balloon_keras.py --hidden-size 100 --cell-size 100 --embedding-size 550 --feature wv --precomputed-features pca250 --learning-window 40 --max-length 40 --k 10 --output outputs/ --name "AA Na15 Balloon 100-100 WV-P ESN-250 keras" --description "Authorship Attribution 15 authors Balloon 100 hidden neurons, Word Vectors (WV) pretrained with Glove, ESN reduced to 250" --rnn-type LSTM --epoch 250 --batch-size 64 --pretrained --verbose 4 --cuda
 
