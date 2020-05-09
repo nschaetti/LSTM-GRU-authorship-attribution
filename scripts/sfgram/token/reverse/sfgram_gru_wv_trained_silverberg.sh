4777c9e590722e191f413f7730d747972505aeb8
nschaetti <n.schaetti@gmail.com>
2020-05-09 10:22:25 +0200

    SFGram dataset

--- scripts/sfgram/token/reverse/sfgram_gru_wv_trained_silverberg.sh
+++ scripts/sfgram/token/reverse/sfgram_gru_wv_trained_silverberg.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+
+# Word pretrained SILVERBERG
+python3 model_rnn_sfgram.py --inverse-dev-test --author0 SILVERBERG --hidden-size 25  --cell-size 25 --embedding-size 100 --feature wv --learning-window 40 --max-length 40 --k 5 --zero-weight 1 --output outputs/sfgram/ --name "SFG SILVERBERG GRU 25-25 WV-T INVERSE" --description "Authorship Verification SILVERBERG SFGram GRU 25 hidden neurons, Word Vectors (WV) trained, inverse" --rnn-type GRU --epoch 100 --batch-size 256 --verbose 4 --cuda
+python3 model_rnn_sfgram.py --inverse-dev-test --author0 SILVERBERG --hidden-size 50  --cell-size 50 --embedding-size 100 --feature wv --learning-window 40 --max-length 40 --k 5 --zero-weight 1 --output outputs/sfgram/ --name "SFG SILVERBERG GRU 50-50 WV-T INVERSE" --description "Authorship Verification SILVERBERG SFGram GRU 50 hidden neurons, Word Vectors (WV) trained, inverse" --rnn-type GRU --epoch 100 --batch-size 256 --verbose 4 --cuda
+python3 model_rnn_sfgram.py --inverse-dev-test --author0 SILVERBERG --hidden-size 100 --cell-size 100 --embedding-size 100 --feature wv --learning-window 40 --max-length 40 --k 5 --zero-weight 1 --output outputs/sfgram/ --name "SFG SILVERBERG GRU 100-100 WV-T INVERSE" --description "Authorship Verification SILVERBERG SFGram GRU 100 hidden neurons, Word Vectors (WV) trained, inverse" --rnn-type GRU --epoch 100 --batch-size 256 --verbose 4 --cuda
+
+
