1ecafce6ccccbcd4d18b664bc9ec44c385fd6465
nschaetti <n.schaetti@gmail.com>
2020-05-13 23:39:07 +0200

    SFGram dataset

---
+++ scripts/sfgram/token/reverse/sfgram_gru_c2_trained_silverberg.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# C2 trained SILVERBERG
+python3 model_rnn_sfgram.py --inverse-dev-test --author0 SILVERBERG --hidden-size 25  --cell-size 25 --embedding-size 30 --feature c2 --learning-window 40 --max-length 40 --k 5 --zero-weight 1 --output-dropout 0.5 --output outputs/sfgram/ --name "SFG SILVERBERG GRU 25-25 C2-T INVERSE" --description "Authorship Verification SILVERBERG SFGram GRU 25 hidden neurons, Character bigram (C2) trained, inverse" --rnn-type GRU --epoch 50 --batch-size 256  --verbose 4 --cuda
+python3 model_rnn_sfgram.py --inverse-dev-test --author0 SILVERBERG --hidden-size 50  --cell-size 50 --embedding-size 30 --feature c2 --learning-window 40 --max-length 40 --k 5 --zero-weight 1 --output-dropout 0.5 --output outputs/sfgram/ --name "SFG SILVERBERG GRU 50-50 C2-T INVERSE" --description "Authorship Verification SILVERBERG SFGram GRU 50 hidden neurons, Character bigram (C2) trained, inverse" --rnn-type GRU --epoch 55 --batch-size 256 --verbose 4 --cuda
+python3 model_rnn_sfgram.py --inverse-dev-test --author0 SILVERBERG --hidden-size 100 --cell-size 100 --embedding-size 30 --feature c2 --learning-window 40 --max-length 40 --k 5 --zero-weight 1 --output-dropout 0.5 --output outputs/sfgram/ --name "SFG SILVERBERG GRU 100-100 C2-T INVERSE" --description "Authorship Verification SILVERBERG SFGram GRU 100 hidden neurons, Character bigram (C2) trained, inverse" --rnn-type GRU --epoch 60 --batch-size 256 --verbose 4 --cuda
+
