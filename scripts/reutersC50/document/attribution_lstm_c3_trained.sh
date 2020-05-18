c4ac4416c27d7bc3be2358971b35581a109bbc0f
nschaetti <n.schaetti@gmail.com>
2020-05-18 21:51:11 +0200

    SFGram dataset

--- scripts/reutersC50/document/attribution_lstm_c3_trained.sh
+++ scripts/reutersC50/document/attribution_lstm_c3_trained.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-python3 model_rnn_keras.py --hidden-size 200 --cell-size 200 --embedding-size 60 --feature c3 --learning-window 40 --max-length 40 --k 10 --output outputs/ --name "AA Na15 LSTM 200-200 C3-60 keras" --description "Authorship Attribution 15 authors LSTM 200 hidden neurons, Character (C3)" --rnn-type LSTM --epoch 140 --batch-size 64 --verbose 4 --cuda
+python3 model_rnn_keras.py --hidden-size 200 --cell-size 200 --embedding-size 30 --feature c3 --learning-window 40 --max-length 40 --k 10 --output outputs/ --name "AA Na15 LSTM 200-200 C3-30 keras" --description "Authorship Attribution 15 authors LSTM 200 hidden neurons, Character (C3)" --rnn-type LSTM --epoch 140 --batch-size 128 --verbose 4 --cuda
 
