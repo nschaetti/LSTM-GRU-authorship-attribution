01d0ef85788fda88133b64ecfccd91b75403f7e7
nschaetti <n.schaetti@gmail.com>
2020-05-19 14:27:36 +0200

    SFGram dataset

--- scripts/reutersC50/document/attribution_gpus2_exp1.sh
+++ scripts/reutersC50/document/attribution_gpus2_exp1.sh
@@ -5,3 +5,4 @@ python3 model_rnn_keras.py --hidden-size 200 --cell-size 200 --embedding-size 30
 
 # GRU-WV trained 50
 python3 model_rnn_keras.py --hidden-size 100 --cell-size 100 --embedding-size 50 --feature wv --learning-window 40 --max-length 40 --k 10 --output output/ --xpname "AA Na15 GRU 100-100 WV-50 keras" --description "Authorship Attribution 15 authors GRU 100 hidden neurons, Word Vectors (WV) trained size 50" --rnn-type GRU --epoch 100 --batch-size 64 --verbose 4 --cuda
+
